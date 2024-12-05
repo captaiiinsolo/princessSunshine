20e92798b42eb460f5239bac4e223be1b9492ea8
captaiiinsolo <solo.d.santos@gmail.com>
2024-12-05 03:05:20 -0800

    Initial commit with project setup

---
+++ README.md
@@ -0,0 +1,36 @@
+# princessSunshine
+
+This project is set up with a React frontend and an Express backend.
+
+## Client
+
+The client-side code is located in the `client` directory and is built with Create React App.
+
+## Server
+
+The server-side code is located in the `server` directory and is built with Express and Mongoose.
+
+## Setup
+
+To set up the project, run the following commands:
+
+```bash
+cd client
+npm install
+cd ../server
+npm install
+```
+
+## Running the Project
+
+To run the client and server, use the following commands:
+
+```bash
+# In one terminal, start the client
+cd client
+npm start
+
+# In another terminal, start the server
+cd server
+npm start
+```
---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Serve static files from React's build directory
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  // Route all other requests to React's index.html
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
+
+const startServer = async (typeDefs, resolvers) => {
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
+};
+
+startServer(typeDefs, resolvers);
